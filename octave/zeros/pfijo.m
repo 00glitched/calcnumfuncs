7cb03e7c3562503ef93e47c59becc592355961e2
00glitched <conaeze3@gmail.com>
2025-03-28 11:51:29 +0000

    update

--- octave/zeros/pfijo.m
+++ octave/zeros/pfijo.m
@@ -1,7 +1,7 @@
 clear;
 pkg load io;
 pkg load struct;
-#pkg load statistics;
+pkg load statistics;
 pkg load optim;
 f = input(  "f(x), put @(x) f(x) : ");
 g = input("x=g(t), put @(x) g(x) : ");
@@ -9,18 +9,18 @@ a = input("A point: ");
 Emax = input("Tolelance: ");
 Imax = input("Max.iter.: ");
 if (Emax>0 && Imax>0)
-	if(abs(deriv(g,a))>1)
-		printf("Diverge, |g'(a)|>1")
-	else
+	#if(abs(deriv(g,a))>1)
+	#	printf("Diverge, |g'(a)|>1")
+	#else
 		p=0;fa=0;fp=0;
 		i=0;e=Emax+1;
 		printf(" n\t | x\t\t | fx\t\t | xi\t\t | fxi\t\t | e\n");
 		while (e>Emax && i<Imax)
 			p  = g(a);
-			fp = f(p);
-			e  = abs(p-a);
+			fp = f(p);fa=f(a);
+			e  = sqrt((p-a)^2);
 			printf(" %i\t | %f\t | %f\t | %f\t | %f\t | %f\n",i,a,fa,p,fp,e);
 			a = p; i += 1;
 		end
-	end
+	#end
 end
